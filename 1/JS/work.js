a884ea8ae1b5acda83a557a0d8980c0a184ff42a
Mila <0632630@ukr.net>
2025-03-12 13:22:01 +0200

    hw1

---
+++ 1/JS/work.js
@@ -0,0 +1,18 @@
+/*Assignment 1*/
+
+let productCount = 16;
+let productName = 'Tomato';
+
+productCount = 20;
+productName = 'Cabage';
+
+/*Assignment 2*/
+
+let userSalary = 900;
+let userPosition = 'Junior Developer'
+
+userSalary = 1500;
+userPosition = 'Middle Developer'
+
+
+
